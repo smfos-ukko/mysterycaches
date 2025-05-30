418567cd73528bf1750708e2315bbfbc01a7e60b
Antti Valkama <73844297+smfos-ukko@users.noreply.github.com>
2025-05-31 01:27:27 +0300

    fix

--- stuff/myst.js
+++ stuff/myst.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     for (let i = 0; i < 4; i++) {
         document.getElementById('puzzleRows').insertAdjacentHTML('beforeend', `
-            <div id="row${i}" class="puzzleRow">
+            <div class="puzzleRow" data-index=${i}>
                 <img src="stuff/i${i}.png">
-                <input id="input${i}" type="text">
+                <input type="text">
                 <span>&#128308;</span>
             </div>
         `);
