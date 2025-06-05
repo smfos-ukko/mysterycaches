93699c557854f62c41a8ac2b38be0f64d38d56be
Antti Valkama <73844297+smfos-ukko@users.noreply.github.com>
2025-06-05 03:08:57 +0300

    fixodus

--- stuff/myst.js
+++ stuff/myst.js
@@ -1,10 +1,17 @@
+const RED = "&#128308;";
+const GREEN = "&#x1f7e2;";
+
+const params = new URLSearchParams(window.location.search);
+const page = params.get('page');
+console.log(page);
+
 window.onload = () => {
     for (let i = 0; i < 4; i++) {
         document.getElementById('puzzleRows').insertAdjacentHTML('beforeend', `
             <div class="puzzleRow" data-index=${i}>
                 <img src="stuff/i${i}.png">
                 <input type="text">
-                <span>&#128308;</span>
+                <span>${RED}</span>
             </div>
         `);
     }
