10761cba5ea0f632ba1cce866970ed3de8d3bea8
Antti Valkama <73844297+smfos-ukko@users.noreply.github.com>
2025-06-06 22:38:18 +0300

    asd

--- stuff/nuorisonturmelus/puzzle.js
+++ stuff/nuorisonturmelus/puzzle.js
@@ -1,12 +1,61 @@
-const RED = "&#128308;";
-const GREEN = "&#x1f7e2;";
+const RED = '🔴';
+const GREEN = '🟢';
 
-for (let i = 0; i < 4; i++) {
+const bids = {
+    0: '1NT - 2C',
+    1: '1NT - 2D - 2H'
+};
+
+const answers = {
+    0: 'stayman',
+    1: 'jacoby'
+};
+
+const checkList = [];
+let flag = 0;
+
+for (let i = 0; i < 2; i++) {
     document.getElementById('puzzleRows').insertAdjacentHTML('beforeend', `
         <div class="puzzleRow" data-index=${i}>
             <img src="stuff/i${i}.png">
-            <input type="text">
-            <span>${RED}</span>
+            <div class="stacked">
+                <h2>${bids[i]}</h2>
+                <div>
+                    <input type="text" class="subtext">
+                    <span>${RED}</span>
+                </div>
+            </div>
         </div>
     `);
+    checkList.push(0);
 }
+
+let textChanged = (e) => {
+    let no = e.target.closest('.puzzleRow').getAttribute('data-index');
+    let bidGuess = e.target.value.toLowerCase();
+    let lamp = e.target.nextElementSibling;
+    if (bidGuess == answers[no]) {
+        lamp.textContent = GREEN;
+        checkList[no] = 1;
+    } else {
+        lamp.textContent = RED;
+        checkList[no] = 0;
+    }
+    if (checkList.every(num => num === 0) && flag == 0) {
+        document.getElementById('puzzleContainer').insertAdjacentHTML('beforeend', `
+            <h2>Milloin meillä?</h2>
+            <div id="finalSet">
+                <input type="text" id="finalText">  
+                <button id="finalButton">Vastaa</button>
+            </div>  
+        `);
+    }
+    flag = 1;
+};
+
+let subs = document.getElementsByClassName('subtext');
+Array.from(subs).forEach((element, index) => {
+    element.addEventListener('input', textChanged);
+});
+
+
